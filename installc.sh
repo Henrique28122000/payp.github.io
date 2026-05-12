606992046e088790e759aca87ad43875ad0e2629
Henrique28122000 <116693914+Henrique28122000@users.noreply.github.com>
2026-05-12 16:36:33 -0300

    Update installc.sh

--- installc.sh
+++ installc.sh
@@ -90,7 +90,7 @@ validate_installation_key() {
     echo -e "${CYAN}📡 Validando chave...${NC}"
     
     # URL base para validação
-    BASE_URL="https://nexyraapi.phfinance.site/netpulse"
+    BASE_URL="https://api.nexyratech.com.br/netpulse"
     
     # Valida a chave e obtém o link base
     RESPONSE=$(curl -s -X POST \
